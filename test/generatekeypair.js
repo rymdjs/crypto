75bcfc345ad96cdf4d6be977e405b562c26005ad
Robin Andersson <k.robin.andersson@gmail.com>
2014-02-24 00:29:18 +0100

    Comment out some test code to make it easier to test data encryption

--- test/generatekeypair.js
+++ test/generatekeypair.js
@@ -13,92 +13,90 @@ describe("#SupportedBrowser: ",function(){
 
 
 
-describe("#Keygenerating: ",function(){
-  it("Chrome: should generate publicKey: ",function(){
-    window.Crypto.generateKeypair().then(
-      function(keypair){
-        keypair.publicKey.should.exist;
-      })
-  })
-  it("Chrome: should generate privateKey: ",function(){
-    window.Crypto.generateKeypair().then(
-      function(keypair){
-        keypair.privateKey.should.exist;
-      })
-  })
-});
-describe("#Keyimporting: ",function(){
-  it("Chrome: should import from Uint8Array: ",function(){
-    key = window.Crypto.Base64.parse(public1);
-    window.Crypto.importKey(key).then(function(key){
-      key.should.exist;
-      key.should.be.a("Uint8Array");
-    }).should.exist;
-  })
-});
-describe("#Keyexporting: ",function(){
-  it("Chrome: should export privateKey to Uint8Array: ",function(){
-    window.Crypto.generateKeypair().then(
-      function(keypair){
-        window.Crypto.exportKey(keypair.privateKey).then(
-          window.Crypto.exportKey(key).then(function(key2){
-            key2.should.exist;
-            key2.should.be.a("Uint8Array");
-          })
-          )})
-  })
-  it("Chrome: should export publicKey to Uint8Array: ",function(){
-    window.Crypto.generateKeypair().then(
-      function(keypair){
-        window.Crypto.exportKey(keypair.publicKey).then(
-          window.Crypto.exportKey(key).then(function(key){
-            key.should.exist;
-            key.be.a("Uint8Array");
-          })
-          )})
-  })
-});
-describe("#Keyconverting: ",function(){
-  it("Chrome: should convert to Base64: ",function(){
-    window.Crypto.Base64.toString(bytes).should.be.a("String");
-    //window.Crypto.Base64.parse(bytes).length.should.be.above(0);
-  })     
-  it("Chrome: should convert from Base64: ",function(){
-    (window.Crypto.Base64.parse(public1)).should.be.a("Uint8Array");
-    })
+// describe("#Keygenerating: ",function(){
+//   it("Chrome: should generate publicKey: ",function(){
+//     window.Crypto.generateKeypair().then(
+//       function(keypair){
+//         keypair.publicKey.should.exist;
+//       })
+//   })
+//   it("Chrome: should generate privateKey: ",function(){
+//     window.Crypto.generateKeypair().then(
+//       function(keypair){
+//         keypair.privateKey.should.exist;
+//       })
+//   })
+// });
+// describe("#Keyimporting: ",function(){
+//   it("Chrome: should import from Uint8Array: ",function(){
+//     key = window.Crypto.Base64.parse(public1);
+//     window.Crypto.importKey(key).then(function(key){
+//       key.should.exist;
+//       key.should.be.a("Uint8Array");
+//     }).should.exist;
+//   })
+// });
+// describe("#Keyexporting: ",function(){
+//   it("Chrome: should export privateKey to Uint8Array: ",function(){
+//     window.Crypto.generateKeypair().then(
+//       function(keypair){
+//         window.Crypto.exportKey(keypair.privateKey).then(
+//           window.Crypto.exportKey(key).then(function(key2){
+//             key2.should.exist;
+//             key2.should.be.a("Uint8Array");
+//           })
+//           )})
+//   })
+//   it("Chrome: should export publicKey to Uint8Array: ",function(){
+//     window.Crypto.generateKeypair().then(
+//       function(keypair){
+//         window.Crypto.exportKey(keypair.publicKey).then(
+//           window.Crypto.exportKey(key).then(function(key){
+//             key.should.exist;
+//             key.be.a("Uint8Array");
+//           })
+//           )})
+//   })
+// });
+// describe("#Keyconverting: ",function(){
+//   it("Chrome: should convert to Base64: ",function(){
+//     window.Crypto.Base64.toString(bytes).should.be.a("String");
+//     //window.Crypto.Base64.parse(bytes).length.should.be.above(0);
+//   })     
+//   it("Chrome: should convert from Base64: ",function(){
+//     (window.Crypto.Base64.parse(public1)).should.be.a("Uint8Array");
+//     })
 
-    //promise.should.be.a("Uint8Array")   
-  it("Chrome: should generate and export publicKey to Uint8Array: ",function(){
-    window.Crypto.generateKeypair().then(
-      function(keypair){
-        window.Crypto.exportKey(keypair.publicKey).then(
-          window.Crypto.exportKey(key).then(
-            function(key){
-            key.should.exist;
-            key.be.a("Uint8Array");
-          })
-    )})
-  })
-    it("Chrome: should generate and export private to Uint8Array: ",function(){
-    window.Crypto.generateKeypair().then(
-      function(keypair){
-        window.Crypto.exportKey(keypair.privateKey).then(
-          window.Crypto.exportKey(key).then(
-            function(key){
-            key.should.exist;
-            //key.be.a("Uint8Array");
-          })
-    )})
+//     //promise.should.be.a("Uint8Array")   
+//   it("Chrome: should generate and export publicKey to Uint8Array: ",function(){
+//     window.Crypto.generateKeypair().then(
+//       function(keypair){
+//         window.Crypto.exportKey(keypair.publicKey).then(
+//           window.Crypto.exportKey(key).then(
+//             function(key){
+//             key.should.exist;
+//             key.be.a("Uint8Array");
+//           })
+//     )})
+//   })
+//     it("Chrome: should generate and export private to Uint8Array: ",function(){
+//     window.Crypto.generateKeypair().then(
+//       function(keypair){
+//         window.Crypto.exportKey(keypair.privateKey).then(
+//           window.Crypto.exportKey(key).then(
+//             function(key){
+//             key.should.exist;
+//             //key.be.a("Uint8Array");
+//           })
+//     )})
+//   })
+// });
 
 describe("#ResourceEncryption: ",function(){
-
   console.log('hit1');
 
   it("Text should be encrypted: ", function(){
-
     console.log('hit2');
-
-
 
     window.Crypto.importKey(window.Crypto.Base64.parse(public1)).then(
 
@@ -109,10 +107,10 @@ describe("#ResourceEncryption: ",function(){
           
           function(encryptedString) {
 
-            console.log('hit4');
-            encryptedString.should.be.a('Integer');
+            encryptedString.should.be.a('String');
           
-          });
+          }
+        );
       }
     );
 
